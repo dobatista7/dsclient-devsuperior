d2f6b5d0e303745eed71f919bac3c085b75a44a6
Douglas Oliveira Batista <dobatista7@gmail.com>
2023-01-31 21:18:49 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
+++ backend/src/main/java/com/devsuperior/dsclient/resources/ClientResource.java
@@ -3,22 +3,25 @@ package com.devsuperior.dsclient.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.services.ClientService;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientResource {
 	
+	@Autowired
+	private ClientService service; 
+	
 	@GetMapping
 	public ResponseEntity<List<Client>> findAll(){
-		List<Client> list = new ArrayList<>();
-		list.add(new Client(1L, "Douglas", "29079654842", 100.0, null, 0));
-		list.add(new Client(2L, "Maria Isabel", "29079654843", 200.0, null, 0));
+		List<Client> list = service.findAll();
 		return ResponseEntity.ok().body(list);		
 		}
 }

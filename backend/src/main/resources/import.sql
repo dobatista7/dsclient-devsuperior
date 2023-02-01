80f485c94bf93db300b250e8a8246e9e273bcee6
Douglas Oliveira Batista <dobatista7@gmail.com>
2023-02-01 11:27:32 -0300

    Data base seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('DOUGLAS OLIVEIRA BATISTA','29079654845',100.0, null,0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('MARIA ISABEL LLORI','29079654846',200.0, null,1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('BEATRIZ LLORI','29079654847',300.0, null,2);

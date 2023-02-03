1dc2db8db74d3a1f111b748310b5e1d8aa8df832
Douglas Oliveira Batista <dobatista7@gmail.com>
2023-02-03 18:23:07 -0300

    Delete Client

--- backend/src/main/java/com/devsuperior/dsclient/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/devsuperior/dsclient/resources/exceptions/ResourceExceptionHandler.java
@@ -2,8 +2,6 @@ package com.devsuperior.dsclient.resources.exceptions;
 
 import java.time.Instant;
 
-import java.time.Instant;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
@@ -11,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.devsuperior.dsclient.services.exceptions.DatabaseException;
 import com.devsuperior.dsclient.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -18,13 +17,29 @@ public class ResourceExceptionHandler {
 	
 	@ExceptionHandler(ResourceNotFoundException.class)
 	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError err = new StandardError();
 		err.setTimestamp(Instant.now());
-		err.setStatus(HttpStatus.NOT_FOUND.value());
+		err.setStatus(status.value());
 		err.setError("Resource not found");
 		err.setMessage(e.getMessage());
 		err.setPath(request.getRequestURI());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);		
+		return ResponseEntity.status(status).body(err);		
 	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError();
+		err.setTimestamp(Instant.now());
+		err.setStatus(status.value());
+		err.setError("Database exception");
+		err.setMessage(e.getMessage());
+		err.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(err);		
+	}
+	
+	
+	
 
 }
